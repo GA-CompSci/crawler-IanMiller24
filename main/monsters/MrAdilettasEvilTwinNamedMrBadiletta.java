6adc0c466aa7dcde368c3ae3c6c99d0135cdbbd9
IanMiller24 <ian.miller@gilmour.org>
2024-11-05 17:17:34 +0000

    done?

--- main/monsters/MrAdilettasEvilTwinNamedMrBadiletta.java
+++ main/monsters/MrAdilettasEvilTwinNamedMrBadiletta.java
@@ -5,14 +5,19 @@ public class MrAdilettasEvilTwinNamedMrBadiletta extends Monster{
     
     //change base health, min/max damage, etc
     public MrAdilettasEvilTwinNamedMrBadiletta(int h, int l, int min, int max){
-        super(h, l, min, max, "Mr. Adiletta's Evil Twin Named Mr. Badiletta", true);
+        super(25, l, min, max, "Mr. Adiletta's Evil Twin Named Mr. Badiletta", true);
         System.out.println("Hey, since when did Mr. A start wearing a monocle and oversized top hat? Wait, that's not Mr. A! That's his evil twin!");
     }
 
     public void attack(){
         //Mr. Badiletta would never use ChatGPT. Only Google Gemini is sufficiently evil for him
         System.out.println("Mr. Badiletta [insert brilliant attack idea he got from Google Gemini]");
-        Player.takeDamage(getDamage());
+        if(Player.health < 30){
+            System.out.println("Mr. Badiletta does extra damage because you're weak!");
+            Player.takeDamage(getDamage() * 2);
+        } else {
+            Player.takeDamage(getDamage());
+        }
     }
 
     public void taunt(){
